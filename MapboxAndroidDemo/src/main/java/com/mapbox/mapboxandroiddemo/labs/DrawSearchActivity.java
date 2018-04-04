bd429b334e1da70df7114958454ba4da10c90730
Langston Smith <langston.smith@mapbox.com>
2018-04-04 13:34:55 -0700

    progress

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/DrawSearchActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/DrawSearchActivity.java
@@ -13,6 +13,7 @@ import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.PolygonOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -50,7 +51,7 @@ public class DrawSearchActivity extends AppCompatActivity implements OnMapReadyC
   private boolean inDrawingMode;
   private boolean inPolygonTapMode;
   private String TAG = "DrawSearchActivity";
-  private List<LatLng> polygon;
+  private List<LatLng> listOfPolygonCoordinates;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +69,7 @@ public class DrawSearchActivity extends AppCompatActivity implements OnMapReadyC
     circleFeatureCollection = FeatureCollection.fromFeatures(circleFeatureList);
     polygonAreaFeatureCollection = FeatureCollection.fromFeatures(polygonFeatureList);
 
-    polygon = new ArrayList<>();
+    listOfPolygonCoordinates = new ArrayList<>();
     anchorPointNum = 0;
 
     mapView = findViewById(R.id.mapView);
@@ -78,6 +79,7 @@ public class DrawSearchActivity extends AppCompatActivity implements OnMapReadyC
 
   @Override
   public void onMapReady(MapboxMap mapboxMap) {
+    Log.d(TAG, "onMapReady: ");
     DrawSearchActivity.this.mapboxMap = mapboxMap;
     setUpCircleSourceAndLayer();
     setUpPolygonSourceAndLayer();
@@ -85,6 +87,7 @@ public class DrawSearchActivity extends AppCompatActivity implements OnMapReadyC
   }
 
   private void setUpPolygonSourceAndLayer() {
+    Log.d(TAG, "setUpPolygonSourceAndLayer: ");
     sourceForSelectedPolygonArea = new GeoJsonSource(SELECTED_SOURCE_GEOJSON_ID);
     mapboxMap.addSource(sourceForSelectedPolygonArea);
     selectedAreaFillLayerPolygon = new FillLayer(SELECTED_SOURCE_LAYER_ID, SELECTED_SOURCE_GEOJSON_ID);
@@ -92,49 +95,73 @@ public class DrawSearchActivity extends AppCompatActivity implements OnMapReadyC
   }
 
   private void setUpCircleSourceAndLayer() {
+    Log.d(TAG, "setUpCircleSourceAndLayer: ");
     sourceForCircleTouchPoints = new GeoJsonSource(CIRCLE_LAYER_GEOJSON_SOURCE_ID);
     mapboxMap.addSource(sourceForCircleTouchPoints);
     circleLayerOfTouchPoints = new CircleLayer(CIRCLE_LAYER_ID, CIRCLE_LAYER_GEOJSON_SOURCE_ID);
-    circleLayerOfTouchPoints.setProperties(
+    circleLayerOfTouchPoints.withProperties(
       circleRadius(9f),
       circleColor(Color.BLUE)
     );
-
     mapboxMap.addLayer(circleLayerOfTouchPoints);
   }
 
   @Override
   public void onMapClick(@NonNull LatLng point) {
-
+    Log.d(TAG, "onMapClick: ");
     addClickPointAsCircleLayerAnchor(point);
   }
 
-
   private void addClickPointAsCircleLayerAnchor(LatLng point) {
-    if (anchorPointNum <= 3) {
-      if (sourceForCircleTouchPoints != null && circleFeatureCollection != null) {
-        if (circleFeatureCollection.features().size() == 0) {
-          Log.d(TAG, "addClickPointAsCircleLayerAnchor: polygonAreaFeatureCollection.features()().size() == 0");
-          circleFeatureCollection = FeatureCollection.fromFeatures(new Feature[] {Feature.fromGeometry(
-            Point.fromLngLat(point.getLongitude(), point.getLatitude()))});
+    Log.d(TAG, "addClickPointAsCircleLayerAnchor: ");
 
-        } else {
-          Log.d(TAG, "addClickPointAsCircleLayerAnchor: polygonAreaFeatureCollection.features()().size() == " +
-            +polygonAreaFeatureCollection.features().size());
+    Log.d(TAG, "addClickPointAsCircleLayerAnchor: click point = " + point.getLatitude());
+    Log.d(TAG, "addClickPointAsCircleLayerAnchor: click point = " + point.getLatitude());
+    /*if (anchorPointNum <= 3) {*/
+    if (sourceForCircleTouchPoints != null && circleFeatureCollection != null) {
 
-          polygonAreaFeatureCollection.features().add(Feature.fromGeometry(
-            Point.fromLngLat(point.getLongitude(), point.getLatitude())));
-        }
-        sourceForCircleTouchPoints.setGeoJson(circleFeatureCollection);
-        anchorPointNum++;
-      }
-    } else {
+
+      circleFeatureCollection = FeatureCollection.fromFeatures(new Feature[] {Feature.fromGeometry(
+        Point.fromLngLat(point.getLongitude(), point.getLatitude()))});
+
+      /*polygonAreaFeatureCollection.features().add(
+        Feature.fromGeometry(
+          Point.fromLngLat(point.getLongitude(), point.getLatitude())));*/
+
+      listOfPolygonCoordinates.add(point);
+      drawPolygon(listOfPolygonCoordinates);
+
+
+//      sourceForCircleTouchPoints.setGeoJson(circleFeatureCollection);
+
+      anchorPointNum++;
+    }
+    /*} else {
+      anchorPointNum = 0;
       mapboxMap.clear();
       circleFeatureCollection.features().clear();
       sourceForCircleTouchPoints.setGeoJson(circleFeatureCollection);
-      anchorPointNum = 0;
-    }
+    }*/
+  }
 
+  private void drawPolygon(List<LatLng> polygonCoordinates) {
+    polygonCoordinates.add(new LatLng(45.522585, -122.685699));
+    polygonCoordinates.add(new LatLng(45.534611, -122.708873));
+    polygonCoordinates.add(new LatLng(45.530883, -122.678833));
+    polygonCoordinates.add(new LatLng(45.547115, -122.667503));
+    polygonCoordinates.add(new LatLng(45.530643, -122.660121));
+    polygonCoordinates.add(new LatLng(45.533529, -122.636260));
+    polygonCoordinates.add(new LatLng(45.521743, -122.659091));
+    polygonCoordinates.add(new LatLng(45.510677, -122.648792));
+    polygonCoordinates.add(new LatLng(45.515008, -122.664070));
+    polygonCoordinates.add(new LatLng(45.502496, -122.669048));
+    polygonCoordinates.add(new LatLng(45.515369, -122.678489));
+    polygonCoordinates.add(new LatLng(45.506346, -122.702007));
+    polygonCoordinates.add(new LatLng(45.522585, -122.685699));
+    mapboxMap.addPolygon(new PolygonOptions()
+      .addAll(polygonCoordinates)
+      .alpha(.5f)
+      .fillColor(Color.parseColor("#4f83ff")));
   }
 
   @Override
